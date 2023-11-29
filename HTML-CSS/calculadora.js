302a6c9a08c409f6be237323cfeff14446d0bc74
Jessica Carolina <jss.c.eng@gmail.com>
2023-11-29 20:10:55 -0300

    calculadora

--- HTML-CSS/calculadora.js
+++ HTML-CSS/calculadora.js
@@ -1,63 +1,71 @@
-function calculadora(){
-    const operação = Number(prompt('escolha uma operação: \n 1 - soma (+) \n 2 - subtração (-) \n 3 - multiplicação (*) \n 4 - divisão real (/) \n 5 - divisão inteira (%) \n 6 - potenciação (**)'));
+function calculadora (){
+    const operação = Number(prompt('escolha uma operação: \n 1 - soma \n 2 - divisão \n 3 - multiplicação'));
 
-    console.log(operação);
-}
-
-//definir as variaveis 
-let n1 = Number(prompt('insira o primeiro valor: '));
-let n2 = Number(prompt('insira o segundo valor: '));
+    if(!operação || operação >=7){
+        alert('erro - operação inválida');
+        calculadora();
+        else{
+            
+            let n1 = Number(prompt('insira o primeiro valor:'));
+let n2 = Number(prompt('insira o segundo valor:'));
 let resultado;
 
-//declarando as funções 
-Function: soma(){
+function soma(){
     resultado = n1 + n2;
-    alert(`${n1} + ${n2} = ${resultado}`)//aqui o alert é pra usar string e numeros juntos, usa crase neste caso
+    alert(`${n1} + ${n2} = ${resultado}`)
+    novaoperação();
 }
 
-Function: subtração(){
+function subtração(){
     resultado = n1 - n2;
-    alert(`${n1} - ${n2} = ${resultado}`) //aqui o alert é pra usar string e numeros juntos, usa crase neste caso
+    alert(`${n1} - ${n2} = ${resultado}`)
+    novaoperação();
 }
 
-Function: multiplicação(){
+function multiplicação(){
     resultado = n1 * n2;
-    alert(`${n1} * ${n2} = ${resultado}`) //aqui o alert é pra usar string e numeros juntos, usa crase neste caso
+    alert(`${n1} * ${n2} = ${resultado}`)
+    novaoperação();
 }
 
-Function: divisãoreal(){
-    resultado = n1 / n2;
-    alert(`${n1} / ${n2} = ${resultado}`) //aqui o alert é pra usar string e numeros juntos, usa crase neste caso
-}
-
-Function: divisãointeira(){
-    resultado = n1 % n2;
-    alert(`o resto da divisão entre ${n1} e ${n2} é igual a ${resultado}`) //aqui o alert é pra usar string e numeros juntos, usa crase neste caso
-}
-
-Function: potenciação(){
-    resultado = n1 ** n2;
-    alert(`${n1} ** ${n2} = ${resultado}`) //aqui o alert é pra usar string e numeros juntos, usa crase neste caso
-}
-//criar agora um verificação de operação usando o if
-
-if (operação == 1){
-    soma();
-    else (operação == 2){
-        subtração();
-        else (operação == 3){
-            multiplicação();
-            else (operação == 4){
-                divisãoreal();
-                else (operação == 5){
-                    divisãointeira();
-                    else (operação == 6){
-                        potenciação();
-                    }
-                }
+function novaoperação(){
+    let opção = prompt('deseja fazer outra operação?:\n 1 - Sim\n 2 - Não');
+    if(opção = 1){
+        calculadora();
+        else if(opção = 2){
+            alert('até mais');
+            else{
+                alert('digite uma opção válida');
+                novaoperação();
             }
         }
     }
 }
+//if(operação = 1){
+   // Soma();
+    //else if (operação = 2){
+       // subtração();
+        // if(operação = 3){
+       //     multiplicação();
+        //}
+    //}
+
+//}
+
+switch (operação){
+    case 1:
+        soma();
+        break;
+        case 2:
+            subtração();
+            break;
+            case 3:
+                multiplicação();
+                break;
+}
+}
+
+        }
+    }
 
 calculadora();
